acc8584e061b72e2bd3cdbe11ac06dfd535a84ef
Hironsan <hiroki.nakayama.py@gmail.com>
2022-10-18 19:23:23 +0900

    Use onnx to persist model

--- asari/api.py
+++ asari/api.py
@@ -1,21 +1,25 @@
 import pathlib
 
-import joblib
-import numpy as np
+import onnxruntime as rt
+
+from asari.preprocess import tokenize
 
 
 class Sonar:
     def __init__(self):
-        pipeline_file = pathlib.Path(__file__).parent / "data" / "pipeline.joblib"
-        self.pipe = joblib.load(pipeline_file)
+        pipeline_file = pathlib.Path(__file__).parent / "data" / "pipeline.onnx"
+        self.sess = rt.InferenceSession(str(pipeline_file))
+        self.input_name = self.sess.get_inputs()[0].name
+        self.prob_name = self.sess.get_outputs()[1].name
 
     def ping(self, text: str):
-        proba = self.pipe.predict_proba([text])[0]
-        mapping = {0: "negative", 1: "positive"}
+        tokenized = tokenize(text)
+        proba = self.sess.run([self.prob_name], {self.input_name: [tokenized]})[0][0]
         res = {
             "text": text,
-            "top_class": mapping[int(np.argmax(proba))],
-            "classes": [{"class_name": mapping[k], "confidence": proba[k]} for k in sorted(mapping)],
+            "top_class": max(proba, key=lambda k: proba[k]),
+            "classes": [
+                {"class_name": class_name, "confidence": confidence} for class_name, confidence in proba.items()
+            ],
         }
-
         return res
--- asari/train.py
+++ asari/train.py
@@ -5,7 +5,8 @@ import argparse
 import json
 import pathlib
 
-import joblib
+import numpy as np
+from skl2onnx import to_onnx
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report
@@ -13,52 +14,63 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 
+from asari.api import Sonar
 from asari.preprocess import tokenize
 
 
 def load_jsonl(filename):
     texts, labels = [], []
-    label_map = {1: "negative", 5: "positive"}
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f:
-            j = json.loads(line)
-            label = int(j["rate"])
-            if label in label_map:
-                texts.append(j["text"])
-                labels.append(label_map[label])
-
+            item = json.loads(line)
+            texts.append(item["text"])
+            labels.append(item["label"])
     return texts, labels
 
 
 def main(args):
     print("Loading dataset...")
     X, y = load_jsonl(args.dataset)
+    X = [tokenize(x) for x in X]
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=42)
 
     print("Fitting...")
     pipe = Pipeline(
         [
-            ("vectorizer", TfidfVectorizer(tokenizer=tokenize, ngram_range=(1, 2))),
+            ("vectorizer", TfidfVectorizer(ngram_range=(1, 2))),
             ("classifier", CalibratedClassifierCV(LinearSVC())),
         ]
     )
     pipe.fit(x_train, y_train)
 
     print("Saving...")
-    joblib.dump(pipe, args.pipeline)
+    seps = {
+        TfidfVectorizer: {
+            "separators": [
+                " ",
+            ],
+        }
+    }
+    onx = to_onnx(pipe, np.array(x_train)[1:], options=seps)
+    with open(args.pipeline, "wb") as f:
+        f.write(onx.SerializeToString())
 
-    if args.test_size > 0.0:
-        print("Predicting...")
-        y_pred = pipe.predict(x_test)
-        print(classification_report(y_test, y_pred, digits=4))
+    print("Predicting...")
+    y_pred = pipe.predict(x_test)
+    print(classification_report(y_test, y_pred, digits=4))
+    print(pipe.predict_proba([tokenize("広告多すぎる♡")]))
+
+    sonar = Sonar()
+    y_pred = [sonar.ping(x)["top_class"] for x in x_test]
+    print(classification_report(y_test, y_pred, digits=4))
 
 
 if __name__ == "__main__":
     DATA_DIR = pathlib.Path(__file__).parent.parent / "data"
     SAVE_DIR = pathlib.Path(__file__).parent / "data"
     parser = argparse.ArgumentParser(description="Training a classifier")
-    parser.add_argument("--dataset", default=DATA_DIR / "reviews.jsonl", help="dataset")
-    parser.add_argument("--pipeline", default=SAVE_DIR / "pipeline.joblib", help="pipeline file")
-    parser.add_argument("--test_size", type=float, default=0.2, help="test data size")
+    parser.add_argument("--dataset", default=DATA_DIR / "dataset.jsonl", help="dataset")
+    parser.add_argument("--pipeline", default=SAVE_DIR / "pipeline.onnx", help="pipeline file")
+    parser.add_argument("--test_size", type=float, default=0.1, help="test data size")
     args = parser.parse_args()
     main(args)
